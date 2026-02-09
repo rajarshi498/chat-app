bf8dff311eb4a37e6423a0deb63b04612a9df04a
Rajarshi Dixit <rajarshi2939@gmail.com>
2026-02-09 12:03:29 +0530

    changed server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,40 +1,43 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { fileURLToPath } from "url";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import path from "path";
-import cors from "cors"
-import {connectDB} from "./lib/db.js";
-import { connect } from "http2";
-import {ENV} from "./lib/env.js"
-import cookieParser from "cookie-parser";
-import {app, server} from "./lib/socket.js";
+import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
+import { app, server } from "./lib/socket.js";
+
 dotenv.config();
 
-app.use(express.json({limit: "5mb"})); // req.body
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
-app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+app.use(cors({
+  origin: ENV.CLIENT_URL,
+  credentials: true
+}));
 
-const PORT = ENV.PORT || 3000;
-const __dirname = path.resolve();
-
-
-
-// app.use("/root",(req,res)=>{
-//     console.log("Root route hit");
-//     return res.json({message:"Welcome to the Chat App API!"}); 
-// });
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(ENV.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
-    app.get("*",(_,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
-    })
-}
+/* ✅ SERVE FRONTEND (ALWAYS) */
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-server.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
-})
+app.get("*", (_, res) => {
+  res.sendFile(
+    path.join(__dirname, "../../frontend/dist/index.html")
+  );
+});
+
+const PORT = ENV.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
